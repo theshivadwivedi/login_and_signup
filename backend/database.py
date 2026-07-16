f192d6a3304ac499b388013a481b9f9e3af6d364
Shiva Dwivedi <dwivedishiva2006@gmail.com>
2026-07-16 18:11:02 +0530

    Refactor database connection to use environment variable
    Load database URL from environment variable using dotenv.

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:Shiva333666@localhost:5432/Login-db"
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 Session_local = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind= engine
+    bind=engine
 )
 
 Base = declarative_base()
-
